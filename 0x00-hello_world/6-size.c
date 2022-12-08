e1759cfd4fea36067d98056fdf777eab0dc652d2
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-08 23:59:07 +0300

    c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
+/*
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float f;
+	int num1;
+	long int num2;
+	long long int num3;
+	char chr1;
+	float num4;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(chr1));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(num1));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(num2));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(num3));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(num4));
 	return (0);
 }
