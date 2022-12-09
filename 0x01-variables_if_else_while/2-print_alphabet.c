da67d1fc9838255a6f8145b3ca8d83db31fdbd80
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-09 17:24:59 +0300

    c file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	char letter = 'a';
 	int counter = 0;
-	
-	while ( counter < 26)
+
+	while (counter < 26)
 	{
 		putchar(letter);
 		counter++;
@@ -18,5 +18,5 @@ int main(void)
 	}
 	putchar('\n');
 
-	return(0);
+	return (0);
 }
