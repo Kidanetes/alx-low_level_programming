8891298eae350b792bb862e14478a96b11885179
kidanetes <kidanemaryamtesfaye@gmail.com>
2022-12-09 06:46:06 -0800

    c file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char lowercas = 'a';
+	int counter = 0;
+
+	while (counter < 26)
+	{
+		if (lowercase == 'e' || lowercase == 'q')
+		{
+			lowercase++;
+			continue;
+		}
+		putchar(lowercase);
+		lowercase++
+		counter++;
+	}
+	
+	return (0);
+}
+
