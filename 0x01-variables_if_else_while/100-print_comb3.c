4ae6e7300ebde7fa8dcfa729acf0e0ad1e4de1e8
kidanetes <kidanemaryamtesfaye@gmail.com>
2022-12-09 14:09:30 -0800

    c file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int counter = 0;
+	int counter2;
+
+	while (counter < 10)
+	{
+		counter2 = counter + 1;
+		while (counter2 < 10)
+		{
+			putchar('0' + counter);
+			putchar('0' + counter2);
+			putchar(',');
+			putchar(' ');
+			counter2++;
+		}
+		counter++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+		
+ 
