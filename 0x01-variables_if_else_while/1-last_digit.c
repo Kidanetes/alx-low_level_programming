32d073635658380cfa17731e5ef0b9b745be0a57
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-09 16:19:50 +0300

    c file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,6 @@
 /* more headers goes there */
 
 /** 
-*betty style doc for function main goes there
 * main - Entry point
 *
 * Return: Always 0 (Success)
@@ -15,12 +14,12 @@ int main(void)
 
   srand(time(0));
   n = rand() - RAND_MAX / 2;
-  /* your code goes there */
+  
   if (n % 10 > 5)
     printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
   else if ( n % 10 == 0)
     printf("Last digit of %d is %d and is 0\n", n, n % 10);
   else 
-     printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10); 
+    printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10); 
   return (0);
 }
