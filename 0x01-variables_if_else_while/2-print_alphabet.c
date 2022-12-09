b27f25d4d9bca920f9acc379b633baad3c96909a
kidanetes <kidanemaryamtesfaye@gmail.com>
2022-12-09 06:18:41 -0800

    c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char letter = 'a';
+	int counter = 0;
+	while (counter < 26 )
+	{
+		putchar(letter);
+		counter++;
+		letter++;
+	}
+	putchar('\n');
+
+	return(0);
+}
