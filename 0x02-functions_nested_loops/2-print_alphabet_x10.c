73e49a72ac861850882ef560c434ad41886f9932
kidanetes <kidanemaryamtesfaye@gmail.com>
2022-12-13 04:39:48 -0800

    c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/*
+ * print_alphabet_x10: prints the lower case alphabet letter 10 times.
+ *
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	int count1 = 0;
+	int count2;
+	char lowercase;
+
+	while (count1 < 10)
+	{
+	 	count2 = 0;
+		while (count2 < 26)
+		{
+			_putchar(lowercase);
+			lowercase++;
+			count2++;
+		}
+		_putchar('\n');
+	}
+
+	return (0);
+}
