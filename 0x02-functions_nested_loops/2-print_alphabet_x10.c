4f5b30c9c8eedda0568ae75bc3620265fc7e157c
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-13 15:54:28 +0300

    c file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /*
- * print_alphabet_x10 - description
- * prints the lower case alphabet letters followed by newline 10 times.
+ * print_alphabet_x10 - prints the lower case alphabet letters
+ * followed by newline 10 times.
  *
  * Return: nothing
  */
