a932c2b2efcfdd264bfb81f3abed399cd2265c14
kidanetes <kidanemaryamtesfaye@gmail.com>
2022-12-13 13:01:57 -0800

    c file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,7 +6,38 @@
  */
 void print_times_table(int n)
 {
+	int i = 0;
+	int j,k,prod;
+	int status;
 
-  }
+	while (i <= n && n >= 0 && n <= 15)
+	{
+		j = 0;
+		while (j <= n)
+		{
+			prod = i * j;
+			k = 100; 
+			status = 0;
+			while(k >= 1)
+			{
+				if ( prod / k != 0 || status > 0)
+				{
+					_putchar('0' + (prod / k);
+					status++;
+				}
+				k /= 10;
+			}
+			_putchar(',);
+			while (status < 4 && j < n)
+			{ 
+				_putchar(' ');
+				status++;
+			}
+			j++;
+		}
+		if (i < n)
+			_putchar('\n');
+			i++;
+	}
 	_putchar('\n');
 }
