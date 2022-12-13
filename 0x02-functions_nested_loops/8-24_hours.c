80c882061835f466464543798946ae9c53bbbbea
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-13 20:45:36 +0300

    c file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,16 +11,18 @@ void jack_bauer(void)
 
 	while (hour < 24)
 	{
-		_putchar('0' + (hour / 10));
-		_putchar('0' + (hour % 10));
 		minute = 0;
 		while (minute < 60)
 		{
+			_putchar('0' + (hour / 10));
+			_putchar('0' + (hour % 10));
+			_putchar(':');
 			_putchar('0' + (minute / 10));
 			_putchar('0' + (minute % 10));
+			_putchar('\n');
 			minute++;
 		}
-		_putchar('\n');
+		
 		hour++;
 	}
 }
