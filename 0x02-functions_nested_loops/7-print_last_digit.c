c53f3c8349f46597f9def8fb4c2cab514cdbcf46
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-13 17:28:38 +0300

    c file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,13 +9,13 @@ int print_last_digit(int n)
 {
 	if (n < 0)
 	{
-		_putchar('0' + (-1 * (n %10)));
-		return (-1 * (n %10));
+		_putchar('0' + (-1 * (n % 10)));
+		return (-1 * (n % 10));
 	}
 	else
 	{
-		_putchar('0' + (n %10));
-		return ((n %10));
+		_putchar('0' + (n % 10));
+		return ((n % 10));
 	}
 		
 	_putchar('\n');
