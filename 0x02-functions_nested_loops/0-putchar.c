e72a85fa92065b909d90f18517f243196ccbbf44
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-13 15:14:51 +0300

    c file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <unistd.h>
 #include "main.h"
 /** main- Entry point
  *
@@ -6,12 +5,12 @@
  */
 int main(void)
 {
-	_putchar(_);
-	_putchar(p);
-	_putchar(u);
-	_putchar(t);
-	_putchar(c);
-	_putchar(h);
-	_putchar(a);
-	_putchar(r);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 }
