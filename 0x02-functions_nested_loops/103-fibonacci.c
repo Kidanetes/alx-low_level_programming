f9d7229b0254384ae30efd3afcc45e937ee77ccf
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-14 02:39:15 +0300

    c file

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,16 +7,16 @@
 int main(void)
 {
 	int tmp, sum = 0, now = 1, prev = 0;
+	
 	while (sum <= 4000000)
 	{
 		tmp = now;
-		if ( now % 2 == 0)
+		if (now % 2 == 0)
 			sum = sum + now;
 		now = now + prev;
 		prev = tmp;
 	}
-	printf("%d\n",sum);
+	printf("%d\n", sum);
 
 	return (0);
 }
-
