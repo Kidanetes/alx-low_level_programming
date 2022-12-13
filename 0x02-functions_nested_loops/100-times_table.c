35cf703bbdc36f6b28be856ba7623288a67aea0f
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-13 23:36:37 +0300

    c file

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_times_table - print  multiplication table 0 - n
+ * @n: input parameter
+ * Return: nothing
+ */
+void print_times_table(int n)
+{
+
+  }
+	_putchar('\n');
+}
