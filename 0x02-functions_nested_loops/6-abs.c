240e2393db86c8d8c269e3b528e283dcf05a44a3
kidanetes <kidanemaryamtesfaye@gmail.com>
2022-12-13 05:57:32 -0800

    c file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absoulute value of n
+ * @n: is an input parameter
+ *
+ * Return: postive integer
+ */
+int _abs(int n)
+{
+	if ( n >= 0)
+		return (n);
+	else
+		return (-n);
+}
