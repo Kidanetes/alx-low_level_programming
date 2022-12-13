6275093e53b1f959d8fe30f7c4e36e83b62b7b36
kidanetes <kidanemaryamtesfaye@gmail.com>
2022-12-13 09:38:02 -0800

    c file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer -  every minute of the day
+ *
+ * Return: nothing
+ */
+void jack_bauer(void)
+{
+	int hour = 0;
+	int minute;
+
+	while (hour < 24)
+	{
+		_putchar('0' + (hour / 10));
+		_putchar('0' + (hour % 10));
+		minute = 0;
+		while (minute < 60)
+		{
+			_putchar('0' + (minute / 10));
+			_putchar('0' + (minute % 10))
+			minute++;
+		}
+		_putchar('\n');
+		hour++;
+	}
+}
