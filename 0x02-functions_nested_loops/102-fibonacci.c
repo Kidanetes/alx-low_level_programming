2aad462b778ef24f384abbd5e60a9e340e13c14c
kidanetes <kidanemaryamtesfaye@gmail.com>
2022-12-13 15:09:07 -0800

    c file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int count =  1, sum = 1, prev = 1;
+	int tmp;
+	while (count <= 50)
+	{
+		tmp = sum;
+		sum = sum + prev;
+		if (count < 50)
+			printf("%d, ", sum);
+		else
+			printf("%d", sum);
+		prev = sum;
+		count++
+	}
+
+	return (0);
+}
+
