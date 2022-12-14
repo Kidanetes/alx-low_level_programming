3e168dd56e6cb0779f220cc311285154709c9f3c
kidanetes <kidanemaryamtesfaye@gmail.com>
2022-12-14 05:54:00 -0800

    c file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,12 +2,13 @@
 /**
  * main - Entry point
  *
- * Return: 0 
+ * return: 0
  */
 int main(void)
 {
-  int i = 0;
-  positive_or_negative(i);
-  
-  return (0);
+	int i = 0;
+	
+	positive_or_negative(i);
+
+	return (0);
 }
