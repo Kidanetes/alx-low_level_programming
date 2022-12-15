b7ebd4f27b6d4cc70801b1908f4232908ad0773c
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-15 17:27:35 +0300

    c file

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_triangle - print a triangle
+ * @size: input parameter
+ * Return: void
+ */
+void print_triangle(int size)(
+{
+	int count = n;
+	int count2;
+
+	while (count > 0)
+	{
+		count2 = 1;
+		while (count2 < count)
+		{
+			_putchar(' ');
+			count2++;
+		}
+		_putchar('#');
+		_putchar('\n');
+		count++;
+	}
+	if (n <= 0)
+		_putchar('\n');
+}
