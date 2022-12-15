b8eb5ae66232d4dc7feb34d4508c2237d4acf0db
kidanetes <kidanemaryamtesfaye@gmail.com>
2022-12-15 06:13:31 -0800

    c file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int count = 1;
+
+	while (count <= 100)
+	{
+		if (count % 3 == 0 && count % 5 == 0)
+			pritnf("FizzBuzz");
+		else if (count % 3 == 0)
+			printf("Fizz");
+		else if (count % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d",count);
+		putchar(' ');
+		count++;
+	}
+	putchar('\n');
+
+	return (0);
+}
