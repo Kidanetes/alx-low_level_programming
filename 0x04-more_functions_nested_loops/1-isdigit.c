9f8e3bc50bfb80bb604c9d214c1689e290919b19
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-15 14:56:39 +0300

    c file

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,9 +5,10 @@
  * @c: is an input parameter
  * Return: 1 if c is digit, 0 otherwise
  */
+
 int _isdigit(int c)
 {
-	if ( c >= '0' && c <= '9')
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
