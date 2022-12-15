dbbb7bf488c3a8cfdd70354ee4960951af657473
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-15 19:18:28 +0300

    c file

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+
 /**
  * main - entry point
  *
@@ -8,7 +8,7 @@
 int main(void)
 {
 	unsigned long int number = 612852475143;
-	unsigned long int count = 2, max = 1;
+	unsigned long int count = 2, count2, max = 1;
 	int status;
 
 	while (count <= (number / 2))
@@ -16,8 +16,18 @@ int main(void)
 		status = 0;
 		if (number % count == 0)
 		{
-			status = check_prime(count);
-			if (status == 1)
+			int status = 0;
+			unsigned long int count2 = 2;
+			while (count2 <= (count / 2))
+			{
+				if (n % count == 0)
+				{
+					status = 1;
+					break;
+				}
+				count2++;
+			}
+			if (status == 0)
 				max = count;
 		}
 		count++;
@@ -25,30 +35,4 @@ int main(void)
 	printf("%lu\n", max);
 
 	return (0);
-}
-/**
- * check_prime - cheks wheter a number is 
- * prime or not
- * @n: input parameter
- * Return: 1 if n is prime, 0 otherwise
- */
-
-int check_prime(unsigned long int n)
-{
-	unsigned long int count = 2;
-	int status = 0;
-
-	while (count <= (n / 2))
-	{
-		if (n % count == 0)
-		{
-			status = 1;
-			break;
-		}
-		count++;
-	}
-	if (status == 0)
-		return (1);
-	else
-		return (0);
 }
