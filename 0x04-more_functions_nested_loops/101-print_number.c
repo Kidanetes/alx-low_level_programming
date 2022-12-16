7f8622560bfa68861c5f984f2fc953e455bade47
kidanetes <kidanemaryamtesfaye@gmail.com>
2022-12-16 10:53:32 -0800

    c file

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * print_number - print a number
+ * only using _putchar
+ * @n: input parameter
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int count = 0;
+	int base = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	int tmp = n;
+	if (n < 9)
+		_putchar('0' + n);
+	else
+	{
+		while (tmp > 0)
+		{
+			tmp = tmp % 10;
+			count++;
+			base = base * 10;
+		}
+		base = base / 10;
+		while (count > 0)
+		{
+			_putchar('0' + (n / base))
+			n = n % base;
+			base = base / 10;
+			count--;
+		}
+	}
+}	
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,3 +10,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 int check_prime(unsigned long int n);
+void print_number(int n);
