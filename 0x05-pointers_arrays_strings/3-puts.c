ccd826bec748a5c3eb34e380f06d198e3321b9d0
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-19 16:42:26 +0300

    c file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @s: input parameter
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+
+	}
+
+	
+}
