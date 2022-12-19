d05402dbd17e8b993ef89c9903154a0a47205074
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-19 16:46:16 +0300

    c file

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
  * _puts - prints a string
- * @s: input parameter
+ * @str: input parameter
  * Return: void
  */
 void _puts(char *str)
 {
 	int i;
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	for (i = 0; *(str + i) != '\0'; i++)
 	{
-		_putchar(*(s + i));
+		_putchar(*(str + i));
 	}
 	_putchar('\n);
 }
