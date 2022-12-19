6f8edeebab2e8bbd6bc1f0388a4e52e8cf8c160a
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-19 16:55:10 +0300

    c file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - prints revrese of a string
+ * @s: input parameter
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int length,i;
+  
+  for (i = 0; *(s + i) != '\0'; i++)
+	{
+	}
+  length = i;
+	for (i = 0; i < length; i++)
+	{
+		_putchar(*(s + (length - 1- i)));
+	}
+	_putchar('\n');
+}
