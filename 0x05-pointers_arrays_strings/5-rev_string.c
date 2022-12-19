7e619c9667eaa0fd463e410b643dfde7a81d3f4f
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-19 21:15:42 +0300

    c file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - revrese a string
+ * @s: input parameter
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int length, i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+	}
+	length = i;
+  char a[length];
+	for (i = 0; i < length; i++)
+	{
+		a[i] = *(s + (length - 1 - i));
+	}
+	*s = &a;
+}
