ae51d4a3d5b54c3651406eef2fa41c656574c2a5
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-19 21:59:38 +0300

    c file

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,10 +12,10 @@ void rev_string(char *s)
 
 	for (i = 0; i < length; i++)
 	{
-		*(a + i) = *(s + (length - 1 - i));
+		*(a + i) = *(*s + (length - 1 - i));
 	}
 	for (i = 0; i < length; i++)
 	{
-		*(s + i) = *(a + i);
+		*(*s + i) = *(a + i);
 	}
 }
