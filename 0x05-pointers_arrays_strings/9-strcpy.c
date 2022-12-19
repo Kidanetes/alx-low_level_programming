63d007344020be5ad414e0d8ed05ed52c2dbcf36
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-19 23:46:33 +0300

    c file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strcp - copies one string into another
+ * @src: input parameter
+ * @dest: input parameter
+ * Return: pointer to destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	*dest = *src
+  return (dest)
+}
