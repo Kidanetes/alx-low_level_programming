f51f7871cd6af720eeb75591ee60d5c437270c68
kidanetes <kidanemaryamtesfaye@gmail.com>
2022-12-19 14:45:16 -0800

    c file

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Entry point
  *
@@ -6,5 +7,13 @@
  */
 int main(void)
 {
+	srand((unsigned) time(&t));
+	char password[10];
+	int i = 10;
+
+	while (i > 0)
+	{
+		password[10 - i] = 32 + rand() % 133;
+	}
 	return (0);
 }
