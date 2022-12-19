8799612809af26c7f3ffc7b1a07aeb0c793fc119
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-20 00:50:26 +0300

    c file

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,14 +16,15 @@ int _atoi(char *s)
 			status = -1 * status;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			tmp = 10 * tmp + ((int) s[i] - (int)'0');
+			if (status == -1)
+				tmp = 10 * tmp - ((int) s[i] - (int)'0');
+			else
+				tmp = 10 * tmp - ((int) s[i] - (int)'0');
 			if (s[i+1] < '0' || s[i+1] > '9')
 				break;
 		}
 		i++;
 	}
-	if (status == -1)
-		tmp = -tmp;
 	return tmp;
 }
 
