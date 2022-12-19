8c92196ac2d52dc3711337bf090a01f7830b81b1
kidanetes <kidanemaryamtesfaye@gmail.com>
2022-12-19 12:39:06 -0800

    c file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,16 +8,16 @@
  */
 void print_array(int *a, int n)
 {
-  int i;
-  
-  for (i = 0; i < n; i++)
-  {
-    printf("%d", a[i]);
-    if (i < n - 1)
-    {
-      putchar(',');
-      putchar(' ');
-    }
-  }
-  putchar('\n');
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 }
