dc6f3782dbc5093250b230671136e3f80a62aa39
kidanetes <kidanemaryamtesfaye@gmail.com>
2022-12-19 05:12:53 -0800

    arrays and pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#incude "main.h"
+/**
+ * swap_int - swap the value of two variables
+ * @a: input parameter
+ * @b: input parameter
+ * return: void
+ */
+void swap_int(int *a, int *b);
+{
+	int tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
