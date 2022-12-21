a4f8767f78e248fda13f389c78125d5492276df6
kidanetes <kidanemaryamtesfaye@gmail.com>
2022-12-21 11:24:51 -0800

    c file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,23 +7,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
- int length1,length2,i;
+	int length1,length2,i;
 
-    for (i = 0; dest[i] != '\0'; i++)
-    {
-
-    }
-    length1 = i;
-    for (i = 0; src[i] != '\0'; i++)
-    {
-
-    }
-    length2 = i;
-    for (i = length1; i < (length1 + length2); i++)
-    {
-
-            dest[i] = src[i-length1];
-    }
-    dest[i] = '\0';
-    return (dest);
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	length1 = i;
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	}
+	length2 = i;
+	for (i = length1; i < (length1 + length2); i++)
+	{
+		dest[i] = src[i-length1];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,29 +3,26 @@
  * _strncat - concatenates two strings into one
  * @dest: input parameter
  * @src: input parameter
- * n: input parameter
+ * @n: input parameter
  * Return: the concatenated string
  */
- char *_strncat(char *dest, char *src, int n)
- {
-    int length1,length2,i;
+char *_strncat(char *dest, char *src, int n)
+{
+	int length1,length2,i;
 
-    for (i = 0; dest[i] != '\0'; i++)
-    {
-
-    }
-    length1 = i;
-    for (i = 0; src[i] != '\0'; i++)
-    {
-
-    }
-    length2 = i;
-    for (i = length1; (i < length1 + n) && (i < (length1 + length2)); i++)
-    {
-
-            dest[i] = src[i-length1];
-    }
-    dest[i] = '\0';
-    return (dest);
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	length1 = i;
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	}
+	length2 = i;
+	for (i = length1; (i < length1 + n) && (i < (length1 + length2)); i++)
+	{
+		dest[i] = src[i-length1];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
