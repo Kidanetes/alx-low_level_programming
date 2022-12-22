34e7194345385885db4690cae0d40491e985d786
kidanetes <kidanemaryamtesfaye@gmail.com>
2022-12-22 10:06:23 -0800

    c file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/** cap_string - capitalize every word of
+ * a string
+ * @s: input parameter
+ * Return: string
+ */
+char *cap_string(char *)
+{
+	int i = 0, status;
+	char* str = {' ', '	', '\n', ',', ';', '.', '!', '?', '\"', '(',')', '{', '}', '\0'};
+
+	while (s[i])
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			status = 0;
+			while (*str && i != 0)
+			{
+				if (s[i - 1] == *str)
+				{
+					status = 1;
+					break;
+				}
+				*str++;
+			}
+			if (status == 1 || i == 0)
+				s[i] = s[i] + ('A' - 'a');
+		}
+		i++;
+	}
+	return (s);
+}
+
+
+
+		
+			
