519b46aefc92aed92a80da51b964fe947092b21c
kidanetes <kidanemaryamtesfaye@gmail.com>
2022-12-22 12:21:06 -0800

    c file

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rot13 - encode the string using
+ * rot13 method
+ * @s: input parameter
+ * Return: the encoded string
+ */
+char *rot13(char *)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z')		{
+			 s[i] = s[i] + 13;
+		}
+		i++;
+	}
+	return (s);
+}
