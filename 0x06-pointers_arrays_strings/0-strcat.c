8fc5472b1676dd045db1644a1393f43c9c8b994d
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-22 15:11:15 +0300

    c file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,8 +5,10 @@
  * @src: input parameter
  * Return: pointer to dest
  */
+
 char* _strcat(char *dest, char *src)
 {
+
 	int i, j;
 
 	for (i = 0; dest[i] != '\0'; i++)
@@ -17,6 +19,5 @@ char* _strcat(char *dest, char *src)
 		dest[i] = src[j];
 	}
 	dest[i] = '\0';
-
-	return dest;
+	return (dest);
 }
