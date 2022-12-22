3aec9ba10b65b50a51145c697d80c9f6aa11efc5
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-22 20:43:03 +0300

    c file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -19,11 +19,15 @@ char *_strncpy(char *dest, char *src, int n)
 	if (n > length)
 	{
 		for (i = length; i < n; i++)
+		{
 			dest[i] = '\0';
+		}
 		n = length;
 	}
 	for (j = 0; j < n; j++)
+	{
 		dest[j] = src[j];
+	}
 
 	return (dest);
 }
