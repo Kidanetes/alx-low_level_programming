d58ef90e42c2aca89e7f183a6b9fad782daf0741
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-23 01:01:20 +0300

    c file

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,27 +6,27 @@
  */
 void print_number(int n)
 {
-	int number_digts = 0;
-	int weight;
+	int count = 0;
+	int base;
 	int status = 0;
 	int tmp;
 
-	float weight_ = 0.1;
+	float base1 = 0.1;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		if (n == -2147483648)
+		if (n != -2147483648)
+		{
+			n = -1 * n;
+		}
+		else
 		{
 			n = -1 * (n + 1);
 			status = 1;
 		}
-		else
-		{
-			n = -1 * n;
-		}
 	}
-	if (n <= '9')
+	if (n <= 9)
 	{
 		_putchar('0' + n);
 	}
@@ -36,24 +36,24 @@ void print_number(int n)
 		while (tmp > 0)
 		{
 			tmp = tmp / 10;
-			number_digts++;
-			weight_ = weight_ * 10.0;
+			count++;
+			base1 = base1 * 10.0;
 		}
-		weight = (int) weight_;
+		base = (int) base1;
 
-		while (number_digts > 0)
+		while (count > 0)
 		{
-			if ((number_digts == 1) && (status == 1))
+			if ((count == 1) && (status == 1))
 			{
-				_putchar('0' + (n / weight + 1));
+				_putchar('0' + (n / base + 1));
 			}
 			else
 			{
-				_putchar('0' + (n / weight));
+				_putchar('0' + (n / base));
 			}
-			n = n % weight;
-			weight = weight / 10;
-			number_digts--;
+			n = n % base;
+			base = base / 10;
+			count--;
 		}
 	}
 }
