fa71825334c47b1c8d2dd759f66974b1d165de7b
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-22 17:02:03 +0300

    c file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,13 +7,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int num = 0, i = 0;
-
+	int num, i = 0;
 	while (s1[i] == s2[i])
 	{
 		i++;
 	}
-	num = s1[i] - s2[i];
+	if (s1[i] == '\0' || s2[i] == '\0')
+		num = 0;
+	else
+		num = s1[i] - s2[i];
 
 	return (num);
 }
