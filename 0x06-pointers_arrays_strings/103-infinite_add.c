d35edd4f268aba4a9ca370277a958f61a6f34eea
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-23 02:49:19 +0300

    c file

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,12 +11,13 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i = 0, j = 0, length1, length2;
 	int res = 0;
-	while (n1)
+	
+	while (n1[i])
 	{
 		i++;
 	}
 	length1 = i;
-	while (n2)
+	while (n2[j])
 	{
 		j++;
 	}
