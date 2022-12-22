b464a4dafaafd9fb74846ac5a2a3aa20f1f04f2a
kidanetes <kidanemaryamtesfaye@gmail.com>
2022-12-22 09:08:28 -0800

    c file

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,12 +7,14 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i = 0, j;
+	int i = 0, j, tmp;
 
 	j = n - 1;
 	while (i <= j)
 	{
+		tmp = a[i];
 		a[i] = a[j];
+		a[j] = tmp;
 		i++;
 		j--;
 	}
