330c6a85b298846704370144b990a4015c9c3a85
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-23 01:34:38 +0300

    c file

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -20,10 +20,6 @@ int main(void)
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
-	/*
-	 * int variables are put in consqutive memory
-	 * one int variable allocate 4 byte
-	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
