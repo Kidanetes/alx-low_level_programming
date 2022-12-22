b8f63c326d7a0922533d4a89bd9b80a89cdf011e
kidanetes <kidanemaryamtesfaye@gmail.com>
2022-12-22 05:54:02 -0800

    c file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: input parameter
+ * @s2: input parameter
+ * Return: Integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int num;
+
+	while (*s1 == *s2)
+	{
+		*s1++;
+		*s2++
+	}
+	num = *s1 - *s2;
+
+	return (num);
+}
