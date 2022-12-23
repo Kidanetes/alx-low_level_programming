543362df615d00aa15fdcbc44058d667eca30afd
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2022-12-23 03:23:03 +0300

    c file

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -43,10 +43,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			}
 			j++;
 		}
-		if (res != 0 && j < size_r)
+		if (res != 0)
 			r[j] = res + '0';
-		else
-			return (0);
 	}
 	r[j] = '\0';
 	return (r);
