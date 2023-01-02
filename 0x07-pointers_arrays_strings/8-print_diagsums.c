332eb3a4af3331e68ed8b873f0de8085068f8e13
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-01-02 13:47:37 -0800

    c file

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagsums - add the sum of the
+ * diagonal of square matrix
+ * @a: input parameter
+ * @size: input parameter
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+			{
+				sum = sum + a[i][j];
+				break;
+			}
+		}
+	}
+	printf("%d", sum);
+}
