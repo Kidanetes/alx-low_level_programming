90f310e64ee5366fecaa11db20882b4ebb030370
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-03 01:16:38 +0300

    c file

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,8 +13,8 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 = sum1 + *(a + i * (size + 1));
-		sum2 = sum2 + *(a + (i + 1) * (size - 1));
+		sum1 = sum1 + *(a + (i * (size + 1)));
+		sum2 = sum2 + *(a + ((i + 1) * (size - 1)));
 	}
 	printf("%d, %d", sum1, sum2);
 }
