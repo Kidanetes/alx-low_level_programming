e22bcb985889160282fb9450df4ebce843e4a8fc
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-01-02 07:18:11 -0800

    c file

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strpbrk - searches a substring
+ * @s: input parameter
+ * @accept: input parameter
+ * Return: pointer to substring
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		while (*accept != '\0')
+		{
+			if (*s == *accept)
+				return (s);
+			accept++;
+		}
+		s++;
+	}
+	return ('\0');
+}	
