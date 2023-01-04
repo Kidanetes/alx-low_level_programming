eacd92dba536d8bb104d063cf4453be30a6165c3
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-01-04 04:18:05 -0800

    c file

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _sqrt_recursion -finds natural square root of a number
+ * @n: input parameter
+ * Return: number
+ */
+int _sqrt_recursion(int n)
+{
+	retun (sqrt_recursion(n, m - 1));
+}
+/**
+ * sqrt_recursion - finds square root of a number
+ * @n: input parameter 1
+ * @m: input parameter 2
+ * Return: number
+ */
+int sqrt_recursion(int n, int m)
+{
+	int x = n;
+	if (m == 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		if (x * x == n)
+			return (x);
+		x = sqrt_recursion(n, m - 1);
+	}
+}
