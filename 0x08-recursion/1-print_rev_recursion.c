5341b475f6c1756e02acc75b734de9589c82ed49
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-01-04 01:56:43 -0800

    c file

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print a reversed string
+ * @s: input parameter
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_putchar(_print_rev_recursion(s));
+	s++;
+}
