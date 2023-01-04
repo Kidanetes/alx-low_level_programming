88308de14e92b85d15e7b5d165f561acb6114ea5
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-01-04 05:44:46 -0800

    c file

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * is_palindrome - checks wheter a string 
+ * is palandiorm or not
+ * @s: input parameter
+ * Return: int( 1 or 0)
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	
+}
