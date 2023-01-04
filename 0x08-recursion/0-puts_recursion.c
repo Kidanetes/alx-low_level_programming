1ac659d9666b812a110eff892762927ea2170910
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-01-04 01:43:10 -0800

    c file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - printa string
+ * @s: input parameter
+ * Return:void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(*s);
+}
