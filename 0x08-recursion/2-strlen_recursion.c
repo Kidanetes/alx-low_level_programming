8e6b403885f6567b12cbe731e95aa58d37ce59c8
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-01-04 03:34:23 -0800

    c file

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: input parameter
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	return (1 + _strlen_recursion(s++));
+}
+
