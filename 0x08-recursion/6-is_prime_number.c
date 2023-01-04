bd7103f9a7bb15f0b70f88191639591a8f2945c2
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-04 15:50:59 +0300

    c file

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -17,16 +17,15 @@ int is_prime_number(int n)
  * Return: 1 if the number is prime,
  * 0, otherwise
  */
-int prime(int n,int m)
+int prime(int n, int m)
 {
 	if (n <= 1 || n >= -1)
 		return (0);
-	else
-	{
-		if (n % m == 0 && n < m)
-			return (0);
-		if (n == m)
-			return (1);
-		return (prime(n, m + 1));
-	}
+	if (n < 0)
+		n = -n;
+	if (n % m == 0 && n < m)
+		return (0);
+	if (n == m)
+		return (1);
+	return (prime(n, m + 1));
 }
