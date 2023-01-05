46a1b4d66d15a105f646f611e79702003eec9113
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-01-04 22:15:25 -0800

    c file

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,7 +7,30 @@
  */
 int is_palindrome(char *s)
 {
+	int x, y;
 	if (*s == '\0')
 		return (1);
+	x = 0;
+	y = _strlen_recursion(s);
+	if (y == 1)
+	{
+		return (1);
+	}
+	else
+		return palindrom(char s, x, y); 
+
 	
+}
+
+int palindrom(char *s, int index, int size)
+{
+	if (index >= size)
+	{
+		return (1);
+	}
+	if (s[index] == s[size])
+	{
+		return (palindrome(s, ++index, --size));
+	}
+	return (0);
 }
