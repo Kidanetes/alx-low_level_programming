bda1f318897f09cdb0247452c74b2552e649e4b0
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-05 09:26:46 +0300

    c file

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * is_palindrome - checks wheter a string 
+ * is_palindrome - checks wheter a string
  * is palandiorm or not
  * @s: input parameter
  * Return: int( 1 or 0)
@@ -8,6 +8,7 @@
 int is_palindrome(char *s)
 {
 	int x, y;
+
 	if (*s == '\0')
 		return (1);
 	x = 0;
@@ -17,9 +18,7 @@ int is_palindrome(char *s)
 		return (1);
 	}
 	else
-		return palindrom(s, x, y - 1); 
-
-	
+		return (palindrom(s, x, y - 1));
 }
 /**
  * palindrom - checks wheter a string is palindrom or not
