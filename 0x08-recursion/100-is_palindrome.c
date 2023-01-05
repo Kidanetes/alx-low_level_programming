ccb39724b4ab8968a54e139365cf1dd27a222a72
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-05 09:24:18 +0300

    c file

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -40,3 +40,17 @@ int palindrom(char *s, int index, int size)
 	}
 	return (0);
 }
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: input parameter
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		s++;
+		return (1 + _strlen_recursion(s));
+	}
+	return (0);
+}
