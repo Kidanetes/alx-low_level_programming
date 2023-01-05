1b6deb7b03b69121730926bfa1784d4b094b338e
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-06 02:10:52 +0300

    c file

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,13 +11,13 @@ int wildcmp(char *s1, char *s2)
 		return (1);
 	if (*s2 == '*')
 	{
-		return (wildcmp(s1, ++s2) || wildcmp(++s1, s2));
+		return (wildcmp(s1, ++s2));
 	}
 	if (*s1 == *s2)
 	{
 		return (wildcmp(++s1, ++s2));
 	}
-	if (*(s2 - 1) == '*' && *s1 != *s2)
+	if (*(s2 - 1) == '*' && *s1 != *s2 && *s2 != '\0' )
 	{
 		return (wildcmp(++s1, s2));
 	}
