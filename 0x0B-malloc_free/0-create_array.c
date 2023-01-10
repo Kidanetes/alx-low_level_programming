6a80129ae27d5d79ac86fd7a38421cb2e9a8de0c
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-10 19:19:18 +0300

    c file

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,11 +15,12 @@ char *create_array(unsigned int size, char c)
 	if (size ==  0)
 		return ('\0');
 	array = malloc(size * sizeof(char));
+	if (*array == '\0')
+		return ('\0');
 	while (i < size)
 	{
 		array[i] = c;
 		i++;
 	}
 	return (array);
-	return ('\0');
 }
