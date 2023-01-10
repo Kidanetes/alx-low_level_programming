ea61763ea3b27f92b61a5f36c383ea37b19d44ad
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-01-10 10:40:20 -0800

    c file

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * str_concat - concatenates two strings
+ * @s1: input parameter1
+ * @s2: input parameter2
+ * Return: concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0, size;
+	char *array;
+
+	for (; s1[i] != '\0'; i++)
+	{
+	}
+	for (; s2[j] != '\0'; j++)
+	{
+	}
+	size = i + j + 1;
+	array = malloc(size);
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		if (i < j)
+			array[i] = s1[i];
+		else
+			array[i] = s2[i - j];
+	}
+	array[i] = '\0';
+	return (array);
+}
