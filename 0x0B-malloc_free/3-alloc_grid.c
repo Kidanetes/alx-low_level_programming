69a2e7b89f7e8f9a58e4e5c9f3680599b2463017
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-11 00:03:24 +0300

    c file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,11 +24,10 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		for(j = 0; j < width; j++)
+		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
 		}
 	}
 	return (grid);
 }
-	
