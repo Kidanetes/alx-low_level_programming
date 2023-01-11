7dbedace8f5f2bac4d3941d09b7dbb298d87447f
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-01-11 02:47:41 -0800

    c file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * argc - input parameter
+ * argv - input parameter
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	int i = 0;
+
+	while (argv[0][i] != '\0')
+	{
+		_putchar(argv[0][i]);
+		i++;
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+project about arguments to main
