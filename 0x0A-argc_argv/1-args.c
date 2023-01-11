fc579c5d3c791dd1bf1c7b4bccb3f6a9d06f77a1
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-01-11 03:06:31 -0800

    c file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: input parameter
+ * @argv: input parameter
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	if (argv[0] != '\0')
+	{
+		printf("%d\n, (argc - 1));
+	}
+	return (0);
+}
