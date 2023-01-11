eb51758e04de53533cf433917f7d1f8571a2a5a3
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-11 14:44:45 +0300

    c file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 		printf("%s\n","Error");
 		return (1);
 	}
-	result = atoi(*(++argv)) * atoi( *(++argv));
+	result = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", result);
 	return (0);
 }
