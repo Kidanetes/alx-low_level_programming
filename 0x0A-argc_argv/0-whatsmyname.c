a9559be0065f26cbd48bdc694b3d9c9324a903f4
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-11 13:59:00 +0300

    c file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * main - Entry point
- * argc - input parameter
- * argv - input parameter
+ * argc: input parameter
+ * argv: input parameter
  * Return: int
  */
 int main(int argc, char *argv[])
