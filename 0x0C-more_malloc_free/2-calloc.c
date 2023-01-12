732981d0e5c6aaf8f0826585b66a7bc7671c2b40
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-12 16:08:03 +0300

    c file

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,6 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb * size; i++)
-		*((char *)(ptr) + i) = 0;
+		/* *((char *)(ptr) + i) = 0;*/
+		*(ptr + i) == (void) 0;
 	return (ptr);
 }
