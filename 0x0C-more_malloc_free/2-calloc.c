0c156fc72ca10ff58180edc065d7c2bb0419cc9e
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-01-12 04:41:13 -0800

    c file

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory for array
+ * @nmemb: input parameter1
+ * @size: input parameter2
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0, j = 0;
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc (nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		ptr[i] = 0;
+	return (ptr);
+}
