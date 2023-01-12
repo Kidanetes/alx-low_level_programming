7fa58670e29c9c5beb4f9b321a115f928c35a86a
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-12 23:28:52 +0300

    c file

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -31,7 +31,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr2);
 	while (i < new_size && i < old_size)
 	{
-		*((char *)ptr2 + i) = *((char *)ptr + i);
+		*((char *)(ptr2) + i) = *((char *)(ptr) + i);
 		i++;
 	}
 	free(ptr);
