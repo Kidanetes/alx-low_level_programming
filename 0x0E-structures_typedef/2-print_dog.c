a6c239eaa96a34c608b59316381b5394dce004b8
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-17 00:02:00 +0300

    c file

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,8 +11,6 @@ void print_dog(struct dog *d)
 	{
 		if ((*d).name == NULL)
 			(*d).name = "(nil)";
-		if ((*d).age == NULL)
-			(*d).age = 0;
 		if ((*d).owner == NULL)
 			(*d).owner = "(nil)";
 		printf("Name: %s\n", (*d).name);
