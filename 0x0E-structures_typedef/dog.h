ddeaf166d0cfc31f6e9ba379a7072c6cae9dc093
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-16 23:29:23 +0300

    header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,7 @@ struct dog
 /**
  * dog_t -  Typedef for new dog structure
  */
+typedef struct dog dog_t;
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
