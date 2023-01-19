b283d10725804e2f1dd406f00cbf8fede40f51e4
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-01-18 17:01:23 -0800

    c file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_name - print a name
+ * @name: input parameter 1
+ * @f: input parameter 2
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f)
+		f();
+}
