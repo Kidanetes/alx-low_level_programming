c0986fdc46ef11f4654d6ac7bef03aa6c53ad527
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-01-22 12:31:05 -0800

    c file

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - print numbers
+ * @separator: imput parameter
+ * @n: number of arguments
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list arg;
+	int j = 0;
+
+	va_start(arg, n);
+	while (i < n)
+	{
+		printf("%d%s",va_arg(arg, int), separator);
+		i++
+	}
+	printf("\n");
+	va_end(arg);
+}
+
+
