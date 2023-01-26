ddb5b832a2700c60db7f9dcba68f929cf54267ed
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-26 13:10:14 +0300

    c file

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,33 +7,36 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	unsigned int j = 0;
-	list_t *new;
-	
-	while (str[j] != '\0')
-		j++;
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *temp;
+	unsigned int length = 0;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
 		return (NULL);
+
+	while (str[length])
+		length++;
+
+	new_node->len = length;
+	new_node->str = strdup(str);
 	if (*head == NULL)
 	{
-		*head = new;
-		(*new).next = NULL;
-		(*new).str = strdup(str);
-		(*new).len = j;
-		return (new);
+		new_node->next = *head;
+		*head = new_node;
 	}
-	new = *head;
-	while ((*new).next != NULL)
+	else
 	{
-		new = (*new).next;
+		new_node->next = NULL;
+		temp = *head;
+		while (temp->next)
+			temp = temp->next;
+		temp->next = new_node;
 	}
-	new = (*new).next;
-	(*new).str = strdup(str);
-	(*new).len = j;
-	(*new).next = NULL;
-	return (new);
+	return (new_node);
 }
+
 
 
 
