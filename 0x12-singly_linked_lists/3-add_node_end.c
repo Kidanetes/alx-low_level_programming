b5a0283303cc4c04a5841a05e89a8df88ff738c0
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-01-26 01:46:15 -0800

    c file

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * add_node_end - add node at the end
+ * @head: head node
+ * @str: string
+ * Return: address to the  new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	unsigned int j = 0;
+	list_t *new;
+
+	while (str[j] != '\0')
+		j++;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		*head = new;
+		(*new).next = NULL;
+		(*new).str = strdup(str);
+		(*new).len = j;
+		return (new);
+	}
+	while ( (*head).next != NULL)
+	{
+		*head = (*head).next;
+	}
+	new = (*head).next;
+	(*new).str = strdup(str);
+	(*new).len = j;
+	(*new).next = NULL;
+	return (new);
+}
+
+
+
