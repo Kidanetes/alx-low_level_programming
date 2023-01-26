bba9aa73efbf0efc4651cd15561c6ec51c52f58e
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-01-26 01:24:49 -0800

    c file

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * add_node - add new node at start
+ * @head: new head
+ * @str: string
+ * Return: address to new head
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int j = 0;
+
+	while (str[j] != '\0')
+		j++;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	(*new).str = strdup(str);
+	(*new).len = j;
+	(*new).next = *head;
+	*head = new;
+	return (*head);
+}
