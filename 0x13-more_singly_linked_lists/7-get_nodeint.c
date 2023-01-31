79e3d8efbcefffab7d0c92c4d4befb95098f6be6
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-31 16:22:11 +0300

    c file

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - find the node at index
+ * @head: input parameter
+ *
+ * Return: the address of the node at index n
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head != NULL)
+	{
+		if (i == index)
+			return (head);
+		head = (*head).next;
+		i++;
+	}
+	return (NULL);
+}
