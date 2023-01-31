a5243787c838bc5788c7939e6d1edbdeac90059b
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-31 15:03:27 +0300

    c file

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,19 +9,12 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *tmp;
-	
+
 	tmp = malloc(sizeof(listint_t));
 	if (tmp == NULL)
 		return (NULL);
-	if (*head == NULL)
-	{
-		(*tmp).n = n;
-		(*tmp).next = NULL;
-		return (*head);
-	}
 	(*tmp).n = n;
 	(*tmp).next = *head;
 	*head = tmp;
 	return (*head);
 }
-
