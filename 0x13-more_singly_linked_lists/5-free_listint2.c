005c0e7ea49efd63b76f5465550a2f9207cfdbed
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-31 15:31:14 +0300

    c file

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a list_t list
+ * @head: head node
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = (*head).next;
+		head = NULL;
+		head = tmp;
+	}
+}
