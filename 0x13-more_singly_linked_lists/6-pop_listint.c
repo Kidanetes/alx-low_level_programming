4547c921e66a7b95dd8e7f6a82c00ff19486d9ec
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-31 16:05:43 +0300

    c file

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * pop_listint - return number of nodes
+ * @head: input parameter
+ *
+ * Return: number of node after deletion
+ */
+int pop_listint(listint_t **head)
+{
+	int tmp = 0;
+
+	if (*head != NULL)
+	{
+		return (tmp);
+	}
+	tmp = (**head).n;
+	*head = (**head).next;
+	return (tmp);
+}
