4406f77065802f69e664efde91064c5ae615b6b0
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-31 15:46:07 +0300

    c file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,13 +7,14 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
+	listint_t *temp;
 
-	while (head != NULL)
+	if (head == NULL)
+		return;
+	while (*head != NULL)
 	{
-		tmp = (*head).next;
-		free(head);
-		head = tmp;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
-	*head = NULL;
 }
