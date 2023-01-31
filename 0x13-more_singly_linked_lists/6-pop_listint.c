63c95586ca7422ae1518d1aa571ae020f37ede66
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-31 16:09:18 +0300

    c file

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,12 +8,15 @@
 int pop_listint(listint_t **head)
 {
 	int tmp = 0;
+	listint_t *tmp2;
 
 	if (*head == NULL)
 	{
 		return (tmp);
 	}
-	tmp = (**head).n;
-	*head = (**head).next;
+	tmp2 = *head;
+	free(head);
+	tmp = (*tmp2).n;
+	*head = (*tmp2).next;
 	return (tmp);
 }
