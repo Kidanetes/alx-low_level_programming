14eddbad67474eefc575a19d7ae8827c58daab7b
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-01-31 16:33:29 +0300

    c file

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * sum_listint - find the sum of all nodes
+ * @head: input parameter
+ * @idx: input parameter
+ * @n: input parameter
+ * Return: address of the new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+}
