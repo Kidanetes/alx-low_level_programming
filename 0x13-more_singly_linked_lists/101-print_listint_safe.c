946c426448e98cab5d641828f2f7ac0966a7ea65
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-02-01 23:04:07 +0300

    c file

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,24 +7,26 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	int num = 0;
-	long int diff;
+	size_t nodes = 0;
+	const listint_t *one = head, *two = head;
 
 	if (head == NULL)
 		exit(98);
-	while (head)
-	{
-		diff = head - head->next;
-		num++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (diff > 0)
-			head = head->next;
-		else
-		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			break;
-		}
-	}
 
-	return (num);
+	while (one && two && two->next && head)
+	{
+		one = one->next;
+		two = two->next->next;
+		if (one == two)
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			exit(98);
+		}
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nodes++;
+	}
+	head = NULL;
+	return (nodes);
 }
