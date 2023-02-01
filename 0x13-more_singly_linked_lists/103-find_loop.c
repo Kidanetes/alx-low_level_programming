4543b36e22acc0c94dd815d667bdaa8e50a8bafa
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-02-01 23:19:34 +0300

    c file

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * find_listint_loop - finds the loop in a list
+ * @head: input parameter
+ * Return: The address of the head, or NULL
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *one = head, *two = head;
+
+	while (one && two && two->next)
+	{
+		one = one->next;
+		two = two->next->next;
+		if (one == two)
+		{
+			return (one);
+		}
+	}
+	return (NULL);
+}
