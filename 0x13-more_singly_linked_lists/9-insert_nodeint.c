6a2edb6097777d45aca364dabf1564b0a16ff8e5
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-02-01 04:40:39 +0300

    c file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,11 +10,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *tmp, *tmp2;
 	unsigned int i = 0;
-	
+
 	tmp = malloc(sizeof(listint_t));
-	
 	if (tmp == NULL)
 		return (NULL);
+	if (*head == NULL && idx == 0)
+	{
+		(*tmp).n = n;
+		(*tmp).next = *head;
+		*head = tmp;
+		return (tmp);
+	}
 	tmp2  = *head;
 	while (tmp2 != NULL)
 	{
