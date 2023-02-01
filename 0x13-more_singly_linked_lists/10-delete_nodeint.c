c86d9fddbfe5a370c2526d1038b95cfde47f2ce7
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-02-01 05:08:06 +0300

    c file

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 	tmp = *head;
-	while (i <= index - 1)
+	while (i < index - 1)
 	{
 		if ((*tmp).next == NULL)
 			return (-1);
