a85fd25a1e193b7a83b09c4146dc5b981c5a0013
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-02-01 05:06:09 +0300

    c file

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - add new node at the head
+ * @head: node
+ * @index: index of deleted node
+ *
+ * Return: 1 or 0 
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tmp, *tmp2;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		free(head);
+		return (1);
+	}
+	tmp = *head;
+	while (i <= index - 1)
+	{
+		if ((*tmp).next == NULL)
+			return (-1);
+		tmp = (*tmp).next;
+		i++;
+	}
+	tmp2 = (*tmp).next;
+	(*tmp).next = (*tmp2).next;
+	free(tmp2);
+	return (1);
+}
+	
+  
