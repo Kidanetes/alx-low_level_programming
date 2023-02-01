173b730d401cd27c3a2088a9772574635ca8150e
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-02-01 22:56:54 +0300

    c file

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * print_listint_safe - prints a list
+ * @head: input parameter
+ *
+ * Return: number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	int num = 0;
+	int diff;
+
+	while (head)
+	{
+		diff = head - head->next;
+		num++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (diff > 0)
+			head = head->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
+	}
+
+	return (num);
+}
