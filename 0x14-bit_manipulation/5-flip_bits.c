1a7a8f742bbf51b255b75ac5146e6d19c62c4b88
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-02-02 23:20:15 +0300

    c file

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * flip bit - set bit at index to 1
+ * flip_bit - count numbe of bits to be filped
  * @n: number
  * @m: number
  * Return: number of bits to fliped
@@ -10,13 +10,13 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int x = n ^ m;
 	unsigned int ones = 0, zeros = 0, i = 0;
 
-	for(i=0; i < 64; i++)
+	for (i = 0; i < 64; i++)
 	{
-		if(x & 1)
+		if (x & 1)
 			ones++;
 		else
 			zeros++;
-		x >>= 1;
+		x = x >> 1;
 	}
 	return (ones);
 }
