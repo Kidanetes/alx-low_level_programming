87afb30668f914a62d0296017278c279146591cf
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-02-02 20:14:22 +0300

    c file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,28 +5,28 @@
  *
  * Return: unsigned int
  */
- unsigned int binary_to_uint(const char *b)
- {
-	 unsigned int i = 0;
-	 unsigned int sum = 0, base;
-	 float weight = 0.5;
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 0;
+	unsigned int sum = 0, base = 1;
 
-	 if (b == NULL)
-		 return (0);
-	 while (b[i] != '\0')
-	 {
-		 if (b[i] != '0' && b[i] != '1')
-			 return (0);
-		 weight = weight * 2;
-		 i++;
-	 }
-	 base = (unsigned int) weight;
-	 i = 0;
-	 while (base != 0)
-	 {
-		 sum = sum + (b[i] - '0') * base;
-		 i++;
-		 base = base / 2;
-	 }
-	 return (sum);
- }
+	if (b == NULL)
+		return (0);
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		i++;
+		if (i != 0)
+			weight = weight * 2;
+	}
+	base = (unsigned int) weight;
+	i = 0;
+	while (base != 0)
+	{
+		sum = sum + (b[i] - '0') * base;
+		i++;
+		base = base / 2;
+	}
+	return (sum);
+}
