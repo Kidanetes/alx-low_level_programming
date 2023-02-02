dabc3c5700cab1d2543da359a7a5d38e6f1fe4c0
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-02-02 23:01:03 +0300

    c file

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,41 +7,8 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int i = 0;
-	unsigned long int base = 1;
-	int array[64];
-
-	if (*n == 0)
-	{
-		array[0] = 0;
-	}
-	else
-	{
-		while (base <= (*n >> 1))
-		{
-			base = base * 2;
-		}
-		while (base != 0)
-		{
-			if (*n >= base)
-			{
-				array[i] = 1;
-				*n = *n - base;
-			}
-			else
-				array[i] = 0;
-			base = base >> 1;
-			i++;
-		}
-		i--;
-	}
 	if (index > 63)
 		return (-1);
-	if (index > i)
-	{
-		array[63 - index] = 1;
-		return (1);
-	}
-	array[i - index] = 1;
-	return (1);	
+	*n |= (1 << index);
+	return (1);
 }
