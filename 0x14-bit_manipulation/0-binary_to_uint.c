652c1c221d3d5f83bc195e35a9f76df3eb050ff6
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-02-02 20:00:43 +0300

    c file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,10 +11,12 @@
 	 unsigned int sum = 0, base;
 	 float weight = 0.5;
 
-	 while (b[i] != NULL)
+	 if (b == NULL)
+		 return (0);
+	 while (b[i] != '\0')
 	 {
 		 if (b[i] != '0' && b[i] != '1')
-			 return (NULL);
+			 return (0);
 		 weight = weight * 2;
 		 i++;
 	 }
