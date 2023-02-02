3f596fb3a07d860a75137d5f61d2b7eab11f86b8
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-02-02 21:30:39 +0300

    c file

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include <main.h>
+/**
+ * print_binary - print binary number
+ * @n: base 10 number
+ *
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int base = 1;
+	int i = 0;
+
+	while (base <= n)
+	{
+		base = base * 2;
+	}
+	if (base > n)
+		base = base >> 1;
+	while (base != 0)
+	{
+		if (n >= base)
+		{
+			_putchar('1');
+			n = n - base;
+		}
+		else
+			_putchar('0');
+		base = base >> 1;
+	}
+}
