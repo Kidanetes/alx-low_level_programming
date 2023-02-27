45c51d3ac84bc9f281ba3136e4bcf0b0a902127b
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-02-27 13:47:46 +0300

    c file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,11 +9,11 @@ int main(void)
 	unsigned long now = 1, prev = 0, tmp;
 	int count = 0;
 
-	while (count < 95)
+	while (count < 96)
 	{
 		tmp =  now;
 		now = now + prev;
-		if (count < 97)
+		if (count < 95)
 			printf("%lu, ", now);
 		else
 			printf("%lu", now);
