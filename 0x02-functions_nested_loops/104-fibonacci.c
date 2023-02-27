eb4574323b03b1998d71f75a5f47353f852b6cd9
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-02-27 14:32:21 +0300

    c file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -22,15 +22,19 @@ int main(void)
 		}
 		else
 		{
-			printf("%lu", now1 + (now2 / 1000000000));
-			if (count < 97)
-				printf("%lu, ", now2 % 1000000000);
-			else
-				printf("%lu\n", now2 % 1000000000);	
 			prev1 = prev / 1000000000;
 			prev2 = prev % 1000000000;
 			now1 = now / 1000000000;
 			now2 = now % 1000000000;
+			printf("%lu", now1 + (now2 / 1000000000));
+			if (count < 97)
+				printf("%lu, ", now2 % 1000000000);
+			else
+				printf("%lu\n", now2 % 1000000000);
+			now1 = now1 + prev1;
+			prev1 = now1 - prev1;
+			now2 = now2 + prev2;
+			prev2 = now2 - prev2;
 			count++;
 		}
 	}
