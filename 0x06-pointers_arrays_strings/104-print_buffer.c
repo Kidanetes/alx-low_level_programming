9684ca3cf8acd4377c9e9e8c91c9b9c6bb77a137
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-02-28 22:17:59 +0300

    c file

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,53 +1,47 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * print_buffer - Prints a buffer 10 bytes at a time, starting with
- *                the byte position, then showing the hex content,
- *                then displaying printable charcaters.
- * @b: The buffer to be printed.
- * @size: The number of bytes to be printed from the buffer.
+ * print_buffer - prints a buffer, 10
+ * bytes per line
+ * @b: buffer pointed by b
+ * @size: size parameter
+ *
+ * Return: nothing
  */
 void print_buffer(char *b, int size)
 {
-	int byte, index;
-
-	for (byte = 0; byte < size; byte += 10)
-	{
-		printf("%08x: ", byte);
-
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size)
-				printf("  ");
-
-			else
-				printf("%02x", *(b + index + byte));
-
-			if ((index % 2) != 0 && index != 0)
-				printf(" ");
-		}
-
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size)
-				break;
-
-			else if (*(b + index + byte) >= 31 &&
-				 *(b + index + byte) <= 126)
-				printf("%c", *(b + index + byte));
-
-			else
-				printf(".");
-		}
-
-		if (byte >= size)
-			continue;
-
-		printf("\n");
-	}
+	int i = 0, j = 0;
+	char tmp;
 
 	if (size <= 0)
 		printf("\n");
+	else
+	{
+		for (; i < size; i = i + 10)
+		{
+			printf("%08x: ", i);
+			for (j = 0; j < 10; j++)
+			{
+				if (j % 2 != 0)
+					printf(" ");
+				tmp = *(b + i + j);
+				if ((i + j) >= size)
+					printf("  ");
+				else
+					printf("%02x", tmp);
+				
+			}
+			for (j = 0; j < 10 ; j++)
+			{
+				tmp = *(b + i + j);
+				if (i + j >= size)
+					break;
+				else if (tmp >= 31 && tmp <= 126)
+					printf("%c", tmp);
+				else
+					printf(".");
+			}
+			printf("\n");
+		}
+	}
 }
-
