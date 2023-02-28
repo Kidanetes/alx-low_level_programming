158b03c4de504bd969986f6d729442f3dae6f15a
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-02-28 14:42:00 +0300

    header file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,3 +12,4 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);	
 void printLnumbers(int n, int s);
+int _strlen(char *s);
