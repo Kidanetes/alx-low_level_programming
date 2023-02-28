e743dc32ac768b395878d52cf971e8cb41b58080
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-02-28 21:24:24 +0300

    c file

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -22,20 +22,20 @@ void print_buffer(char *b, int size)
 			printf("%08x:", i);
 			for (; j < 10; j++)
 			{
-				if (i + j > size)
+				if (i + j >= size)
 					printf(" ");
 				else
 					printf("%02x", *(b + i + j));
 				if (j % 2 == 0)
 					printf(" ");
 			}
-			for (; j < 10 ; j++)
+			for (j = 0; j < 10 ; j++)
 			{
 				tmp = *(b + i + j);
-				if (i + j > size)
+				if (i + j >= size)
 					break;
 				else if (tmp >= 32 && tmp <= 126)
-					printf("%c", *(b + i + j));
+					printf("%c", tmp);
 				else
 					printf(".");
 			}
