77c9272ebcec0d306109dcc42c0eceefb84f13f9
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-03-01 00:11:02 +0300

    c file

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,13 +8,11 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s)
 	{
 		if (*s == c)
 			return (s);
 		s++;
 	}
-	if (*s == c)
-		return (s);
-	return ('\0');
+	return (NULL);
 }
