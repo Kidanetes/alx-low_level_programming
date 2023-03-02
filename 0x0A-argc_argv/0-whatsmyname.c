364fa4b50b4d15bf993c7899e0f616b64d8639b3
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-03-02 23:47:17 +0300

    program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,12 @@
 #include "main.h"
 #include <stdio.h>
-int __attribute__ ((unused)) argc;
 /**
  * main - Entry point
  * @argc: input parameter
  * @argv: input parameter
  * Return: int
  */
-int main(int argc, char *argv[])
+int main(int __attribute__ ((unused)) argc, char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
