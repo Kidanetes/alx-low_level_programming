fab080c9b4cb5d2d278bb0a076cb4605212e0222
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-03-02 23:39:34 +0300

    program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,16 +7,6 @@
  */
 int main(int argc, char *argv[])
 {
-	int i = 0;
-
-	if (argc > 0)
-	{
-		while (argv[0][i] != '\0')
-		{
-			_putchar(argv[0][i]);
-			i++;
-		}
-	}
-	_putchar('\n');
+	printf("%s\n", *argv);
 	return (0);
 }
