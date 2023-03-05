01f27d9f90f736135d6a2a65a3ffed6f8ae1328d
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-03-06 01:20:57 +0300

    c file

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * strtow - split a string into words
+ * @str: the string
+ *
+ * Return: double pointer
+ */
+char **strtow(char *str)
+{
+	int x = 0, i, j, k, length;
+	char **strtow;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+			x++;
+	}
+	x++;
+	strtow = malloc(x);
+	if (strtow == NULL)
+		return (NULL);
+	j = 0;
+	for (i = 0; i < x; i++)
+	{
+		length = 0;
+		while (s[j]  != ' ' && s[j] != '\0')
+		{
+			length++;
+		}
+		if (length != 0)
+		{
+			strtow[i] = malloc(length + 1);
+			i++;
+		}
+		if (strtow[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(strtow[i]);
+			free(strtow);
+			return (NULL);
+		}
+		j++;
+	}
+	j = 0;
+	for (i = 0; i < x; i++)
+	{
+		k = 0;
+		while (*s != ' ' && *s != '\0')
+		{
+			strtow[i][k] = *str;
+			str++;
+			k++;
+		}
+		while (*str == ' ')
+			str++;
+		i++;
+	}
+	return (strtow);
+}
+	
+	
+	
+ 
