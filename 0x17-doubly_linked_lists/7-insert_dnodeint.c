71ab591a5422a46f0637957eb3b9210f41bb4e3f
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-03-17 01:29:17 +0300

    c file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 
-	if (*h == NULL && idx == 0)
+	if (idx == 0)
 	{
 		new = add_dnodeint(h, n);
 		return (new);
