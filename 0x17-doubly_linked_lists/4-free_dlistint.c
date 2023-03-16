7c7b903180ebf0d95cc3067a51708a2c1f525558
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-03-16 14:15:34 -0700

    c file

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint - free a double linked list
+ * @head: head of the DLL
+ *
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while(head != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
