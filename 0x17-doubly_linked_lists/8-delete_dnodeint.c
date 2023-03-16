49c943dc1194df5f0474d8955d86b62d2c016ae6
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-03-17 02:32:02 +0300

    c file

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,11 +10,14 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *tmp1 = *head, *tmp2;
 	dlistint_t *tmp3;
 	unsigned int i = 0;
-
-	if (*head != NULL && index == 0)
+	
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
 	{
 		tmp1 = *head;
 		*head = (**head).next;
+		free(tmp1);
 		return (1);
 
 	}
@@ -44,4 +47,3 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	return (-1);
 }
-
