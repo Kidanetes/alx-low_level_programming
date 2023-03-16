1df13ffa5a8493e9bdf3398e6b50bf38a6096f28
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-03-17 01:38:51 +0300

    c file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,8 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-		return (NULL)
-
+		return (NULL);
 	if (idx == 0)
 	{
 		new = add_dnodeint(h, n);
