fbb9d3f2ad7c6c8d2376d716398141106dde8cab
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-03-21 05:01:36 +0300

    shell script

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/Kidanetes/alx-low_level_programming/blob/main/0x18-dynamic_libraries/liball.so
-export LD_PRELOAD="$PWD/../liball.so"
+wget -P .. https://github.com/Kidanetes/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libdynamic.so
+export LD_PRELOAD="$PWD/../libdynamic.so"
