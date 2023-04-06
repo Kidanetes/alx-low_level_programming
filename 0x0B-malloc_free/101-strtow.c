a69f9c2b642cdb06d7c5876726591ef7e339b774
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-04-06 04:06:07 -0700

    malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * strtow - split a string into words
  * @str: the string
@@ -8,60 +9,59 @@
  */
 char **strtow(char *str)
 {
-	int x = 1, i, j, k, length;
+	int x = 1, i = 0, j, k, length;
 	char **strtow;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
-	for (i = 1; str[i] != '\0'; i++)
+	while (str[i] == ' ')
+		i++;
+
+	for (; str[i] != '\0'; i++)
 	{
+		
 		if (str[i] == ' ')
 		{
+			if (str[i + 1] == ' ' || str[i + 1] == '\0')
+				continue;
 			x++;
 		}
 	}
-	x++;
-	strtow = malloc(x);
+	strtow = malloc((x + 1) * sizeof(char *));
 	if (strtow == NULL)
 		return (NULL);
-	j = 0;
 	i = 0;
+	j = 0;
 	while (i < x)
 	{
 		length = 0;
-		while (str[j]  != ' ' && str[j] != '\0')
+		while (str[j] == ' ')
+                	j++;
+		k = j;
+		while (str[k] != ' ' && str[k] != '\0')
 		{
 			length++;
-			j++;
+			k++;
 		}
-		if (length != 0)
+		strtow[i] = malloc(length + 1);
+		if (strtow[i] == NULL)
 		{
-			strtow[i] = malloc(length + 1);
-			if (strtow[i] == NULL)
-			{
-				for (; i >= 0; i--)
-					free(strtow[i]);
-				free(strtow);
-				return (NULL);
-			}
-			i++;
+			while (i--)
+				free(strtow[i]);
+			free(strtow);
+			return (NULL);
 		}
-		j++;
-	}
-	j = 0;
-	for (i = 0; i < x; i++)
-	{
 		k = 0;
-		while (*str != ' ' && *str != '\0')
+		length = j + length;
+		while (j < length)
 		{
-			strtow[i][k] = *str;
-			str++;
+			strtow[i][k] = str[j];
+			j++;
 			k++;
 		}
 		strtow[i][k] = '\0';
-		while (*str == ' ')
-			str++;
 		i++;
 	}
+	strtow[i] = NULL;
 	return (strtow);
 }
