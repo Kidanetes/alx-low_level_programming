dab43f0ee13f0f6b607c5f31b18f73d49546def9
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-04-30 22:37:01 +0300

    string concatenation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,28 +10,27 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i = 0, j = 0, k = 0;
-        char *s3;
+	char *s3;
 
-        if (s1 == NULL)
-                s1 = "";
-        if (s2 == NULL)
-                s2 = "";
-        while (s1[i])
-                i++;
-        while (s2[j] && j < n)
-                j++;
-        s3 = malloc((i + j + 1) * sizeof(char));
-
-        if (s3 == NULL)
-                return (NULL);
-        while (k < (i + j))
-        {
-                if (k < i)
-                        s3[k] = s1[k];
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i])
+		i++;
+	while (s2[j] && j < n)
+		j++;
+	s3 = malloc((i + j + 1) * sizeof(char));
+	if (s3 == NULL)
+		return (NULL);
+	while (k < (i + j))
+	{
+		if (k < i)
+			s3[k] = s1[k];
 		else
-                        s3[k] = s2[k - i];
-                k++;
-        }
-        s3[k] = '\0';
-        return (s3);
+			s3[k] = s2[k - i];
+		k++;
+	}
+	s3[k] = '\0';
+	return (s3);
 }
