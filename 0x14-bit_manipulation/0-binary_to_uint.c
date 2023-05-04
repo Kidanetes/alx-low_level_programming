f5810617926441c58cadec9958bdee9fb2b2eafa
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-05-04 14:49:10 -0700

    base 10 to integer

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * binary_to_uint - convet binary to unsigned int
+ * @b: binary string
+ *
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 0;
+	unsigned int sum = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		sum = sum * 2 + (b[i] - '0');
+		i++;
+	}
+	return (sum);
+}
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_binary - print binary number
+ * @n: base 10 number
+ *
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int base = 1;
+
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		while (base <= (n >> 1))
+		{
+			base = base * 2;
+		}
+		while (base != 0)
+		{
+			if (n >= base)
+			{
+				_putchar('1');
+				n = n - base;
+			}
+			else
+				_putchar('0');
+			base = base >> 1;
+		}
+	}
+}
