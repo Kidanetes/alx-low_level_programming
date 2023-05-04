c1539003b7f8579dcfcc7931e9d1a3187ea60630
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-05-05 00:55:04 +0300

    bit manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * get_endianness - check wheter it is big endian or
+ * little endian
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *) &i;
+
+	if (*c)
+		return (1);
+	return (0);
+}
