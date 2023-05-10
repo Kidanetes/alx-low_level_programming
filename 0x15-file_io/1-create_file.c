d4f86a058ebe8bb981f8f58aa3c7ec370af5adfd
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-05-10 15:51:51 +0300

    correcting syntax error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	i = open(filename, O_RDWR | O_CREATE | O_TRUNC, 0600);
+	i = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (i == -1)
 		return (-1);
 	if (text_content != NULL)
