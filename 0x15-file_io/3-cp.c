550cd0a814c820cbaec6d1b799020f518e5b5e8d
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-05-12 00:34:20 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	j = open(argv[2], O_WRONLY | O_APPEND | O_TRUNC, 0664);
+	j = open(argv[2], O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, 0664);
 	if (j == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
