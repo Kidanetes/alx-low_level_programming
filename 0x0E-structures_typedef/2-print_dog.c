e8b77d179a3c74f6829dc433667bb725069e8b1b
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-06-18 16:22:39 +0300

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,11 +12,11 @@ void print_dog(struct dog *d)
 		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
 		else
-			printf("Name: (nil)");
+			printf("Name: (nil)\n");
 		printf("Age: %.1f\n", d->age);
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("Owner: (nil)");
+			printf("Owner: (nil)\n");
 	}
 }
