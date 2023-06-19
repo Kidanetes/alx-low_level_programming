5bf5a357ade635c65066db76de1aba25eb4523bc
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-06-19 05:14:30 -0700

    structure and typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -36,7 +36,7 @@ dog_t *new_dog(char *name, float age, char *owner)
         new_dog->name = name;
         new_dog->age = age;
         new_dog->owner = owner;
-        return (new);
+        return (new_dog);
 	
 }
 /**
