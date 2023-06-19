26408b2f5bb312a5374a32762bcaea3cd1e4a5da
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-06-19 13:07:35 -0700

    creating dynamic library to be used frm python

---
+++ 0x18-dynamic_libraries/cfile5.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @a: input 1
+ * @b: input 2
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - sub two integers
+ * @a: input 1
+ * @b: input 2
+ * Return: differnce
+ */
+int sub(int a, int b)
+{
+        return (a - b);
+}
+/**
+ * mul - mul two integers
+ * @a: input 1
+ * @b: input 2
+ * Return: product
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}
+/**
+ * div - div two integers
+ * @a: input 1
+ * @b: input 2
+ * Return: quotient
+ */
+int div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * mod - add two integers
+ * @a: input 1
+ * @b: input 2
+ * Return: mod
+ */
+int mod(int a, int b)
+{
+        return (a % b);
+}
