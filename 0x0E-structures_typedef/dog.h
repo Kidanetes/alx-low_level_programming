c18bfccee5444e281c6d22a07cd1931d50c2390b
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-06-19 15:03:02 +0300

    updating header file to include new function defintion

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,5 +20,6 @@ typedef struct dog dog_t;
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
+char *copy(char *name);
 void free_dog(dog_t *d);
 #endif
