1eda85a2909ce2dc2ec66c31ef32303ef1f2ced9
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-07-06 14:20:04 -0700

    adding key:value pair to hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * hash_table_set - add to hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value
+ * Return: index
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *ptr;
+	unsigned long int index;
+
+	if (strlen(key) == 0)
+		return (0);
+
+	ptr = malloc(sizeof(hash_node_t));
+	if (ptr == NULL)
+		return (0);
+	ptr->key = strdup(key);
+	ptr->value = strdup(value);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index] == NULL)
+		ht->array[index] = ptr;
+	else
+	{
+		while ((ht->array[index])->next != NULL)
+			ht->array[index] = (ht->array[index])->next;
+		(ht->array[index])->next = ptr;
+		ptr->next = NULL;
+	}
+	return (1);
+}
+
+
