7eff456d9ad94d13f461a57ebb1e7d1de4b5a3ef
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-07-06 14:24:45 -0700

    adding key:value pair to hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,7 +23,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	ptr->value = strdup(value);
 	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
+	{
 		ht->array[index] = ptr;
+		ptr->next = NULL;
+	}
 	else
 	{
 		while ((ht->array[index])->next != NULL)
