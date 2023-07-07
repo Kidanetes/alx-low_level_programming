dca268f674c5485e8869af850d169e32f3f69d2a
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-07-07 15:17:35 -0700

    free a hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+/**
+ * hash_table_delete - delete a has table
+ * @ht:  hash table
+ *
+ * Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *ptr, *tmp;
+
+	if (ht != NULL)
+	{
+		for (i = 0; i < ht->size; i++)
+		{
+			ptr = ht->array[i];
+			while (ptr != NULL)
+			{
+				tmp = ptr->next;
+				free(ptr->value);
+				free(ptr->key);
+				free(ptr);
+				ptr = tmp;
+			}
+		}
+		free(ht->array);
+		free(ht);
+	}
+}
