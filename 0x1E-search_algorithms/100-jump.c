2981fdfd5c8451e958bbc947cac5370a8bd4840b
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-05-21 01:25:01 +0300

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -12,7 +12,7 @@ int jump_search(int *array, size_t size, int value)
 	int n = (int)sqrt(size);
 
 	j = i + n;
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	while (i < (int)size)
