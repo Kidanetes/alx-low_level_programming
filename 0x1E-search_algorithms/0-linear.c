f35820c3f53513f4447b1f2e41575afc5c9b0aa5
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-05-20 19:03:56 +0300

    betty fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,11 +1,15 @@
 #include "search_algos.h"
 /**
- *
- *
+ * linear_search - search using linear aligorithm
+ * @array: array to be searched
+ * @size: size of the array
+ * @value: value to be searched
+ * Return: index(if found) or -1(if not found)
  */
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
+
 	if (array == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
