3973d00f4287630d6c2246e44366b7e441e546d4
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-05-21 00:46:35 +0300

    jump search aligorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -11,22 +11,22 @@ int jump_search(int *array, size_t size, int value)
 	int i = 0, j;
 	int n = (int)sqrt(size);
 
+	j = i + n;
 	if (array == NULL)
 		return (-1);
-	if (size == 1)
+	/*if (size == 1)
 	{
 		printf("Value checked array[0] = [%d]\n", array[0]);
 		if (array[0] == value)
 			return (0);
 		return (-1);
-	}
+	}*/
 	while (i < (int)size)
 	{
-		j = i + n;
 		if (array[i] > value)
 			return (-1);
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		if (array[j] >= value)
+		if (array[j] >= value || j >= (int)size)
 		{
 			printf("Value found between indexes [%d] and [%d]\n", i, j);
 			break;
